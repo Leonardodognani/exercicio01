51c0e025e55c68165e5976d3e4cd3b2b8769f399
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-02-08 11:58:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//crie um algoritmo que retorna se um número é par ou ímpar.
+
+namespace exercicio01
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Olá! Por favor, digite um número: ");
+            int numeroDigitado;
+            numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroDigitado % 2 == 0)
+            {
+                Console.WriteLine($"O número {numeroDigitado} é um número Par.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numeroDigitado} é um número ímpar.");
+            }
+
+
+        }
+    }
+}
